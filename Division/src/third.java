5a365dbb6db9361d6d67705f211e6be5f665be20
srikanthc296 <srikanthc296@github.com>
2019-02-22 14:52:49 -0500

    commit

---
+++ Division/src/third.java
@@ -0,0 +1,14 @@
+
+public class third {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c;
+		a = 6;
+		b = 3;
+		c = a/b ;
+		System.out.println(c);
+
+	}
+
+}
